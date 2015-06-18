0b12cb4c155171586705c9820484c58b8229c26b
Dave McPherson <dmcp89@gmail.com>
2015-06-18 19:49:53 -0400

    adding artist list item layout

--- app/src/main/java/com/davemcpherson/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/davemcpherson/spotifystreamer/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
 
 
 /**
@@ -18,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String temp[] ={"Test Artist"};
+
+        ArrayAdapter<String> mArtistAdapter= new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.artist_search_item,
+                R.id.ArtistNameTxt,
+                Arrays.asList(temp));
+
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) root.findViewById(R.id.ArtistList);
+        listView.setAdapter(mArtistAdapter);
+
+        return root;
     }
 }
