9cb8f3883390e784159468a6c60bd798b6f34e43
Dave McPherson <dmcp89@gmail.com>
2015-06-25 22:33:09 -0400

    Commit

--- app/src/main/java/com/davemcpherson/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/davemcpherson/spotifystreamer/MainActivityFragment.java
@@ -3,7 +3,6 @@ package com.davemcpherson.spotifystreamer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +15,7 @@ import java.util.Arrays;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Artist;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
@@ -54,17 +49,8 @@ public class MainActivityFragment extends Fragment implements OnItemClickListene
 
         SpotifyApi api = new SpotifyApi();
         SpotifyService spotify = api.getService();
-        spotify.getArtist("4gzpq5DPGxSnKTe4SA8HAU", new Callback<Artist>() {
-            @Override
-            public void success(Artist artist, Response response) {
-                Log.i("Spotify","Artist: "+artist.name);
-            }
+        ArtistsPager AP = spotify.searchArtists("coldplay");
 
-            @Override
-            public void failure(RetrofitError error) {
-                Log.i("Spotify",error.toString());
-            }
-        });
 
         return root;
     }
---
+++ app/src/main/java/com/davemcpherson/spotifystreamer/tasks/SearchArtistTask.java
@@ -0,0 +1,7 @@
+package com.davemcpherson.spotifystreamer.tasks;
+
+/**
+ * Created by dave on 6/25/2015.
+ */
+public class SearchArtistTask {
+}
