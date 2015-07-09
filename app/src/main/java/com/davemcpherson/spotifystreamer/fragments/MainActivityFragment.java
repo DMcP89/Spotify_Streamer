ca801ae86821ce504e3e1ee9b7ac48fe1e15c753
Dave McPherson <dmcp89@gmail.com>
2015-07-08 21:59:25 -0400

    implemented artist search from edittext content

--- app/src/main/java/com/davemcpherson/spotifystreamer/fragments/MainActivityFragment.java
+++ app/src/main/java/com/davemcpherson/spotifystreamer/fragments/MainActivityFragment.java
@@ -3,12 +3,15 @@ package com.davemcpherson.spotifystreamer.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.davemcpherson.spotifystreamer.R;
@@ -26,7 +29,10 @@ import kaaes.spotify.webapi.android.SpotifyService;
  */
 public class MainActivityFragment extends Fragment implements OnItemClickListener {
 
-	private FragmentManager fragMan;    
+	private FragmentManager fragMan;
+    private ListView artistList;
+    private EditText searchText;
+    private SpotifyService spotifyService;
 
 	public MainActivityFragment() {
     }
@@ -36,7 +42,7 @@ public class MainActivityFragment extends Fragment implements OnItemClickListene
                              Bundle savedInstanceState) {
 
 
-        String temp[] ={"Test Artist"};
+        String temp[] ={};
 
         ArrayAdapter<String> mArtistAdapter= new ArrayAdapter<String>(
                 getActivity(),
@@ -45,15 +51,16 @@ public class MainActivityFragment extends Fragment implements OnItemClickListene
                 Arrays.asList(temp));
 
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        ListView listView = (ListView) root.findViewById(R.id.ArtistList);
-        //listView.setAdapter(mArtistAdapter);
-		listView.setOnItemClickListener(this);
 
+        artistList = (ListView) root.findViewById(R.id.ArtistList);
+        artistList.setAdapter(mArtistAdapter);
+        artistList.setOnItemClickListener(this);
 
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotifyService = api.getService();
-        SearchArtistTask task = new SearchArtistTask(new SearchArtistCommand(spotifyService), listView);
-        task.execute("coldplay");
+        searchText = (EditText) root.findViewById(R.id.SearchArtistTxt);
+        searchText.addTextChangedListener(new SearchTextWatcher());
+        spotifyService = new SpotifyApi().getService();
+
+        //task.execute("coldplay");
         /*try {
             ArtistsPager ap = (ArtistsPager)task.get();
             ArtisitAdapter artisitAdapter = new ArtisitAdapter(getActivity(),ap.artists.items);
@@ -75,6 +82,27 @@ public class MainActivityFragment extends Fragment implements OnItemClickListene
 			.replace(this.getId(), new TopTracksFragment(""))
 			.commit();
 	}
+
+
+    private class SearchTextWatcher implements TextWatcher{
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //Toast.makeText(getActivity(), s.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //Toast.makeText(getActivity(), s.toString(), Toast.LENGTH_LONG).show();
+            SearchArtistTask task = new SearchArtistTask(new SearchArtistCommand(spotifyService), artistList);
+            task.execute(s.toString());
+        }
+    }
 
 	
 	
