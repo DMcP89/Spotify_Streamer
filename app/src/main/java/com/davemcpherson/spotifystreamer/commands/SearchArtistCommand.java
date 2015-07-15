2203f9139d0180331b59e58866b8c6ef5e217703
Dave McPherson <dmcp89@gmail.com>
2015-07-14 23:40:50 -0400

    refactored command classes to use async api methods

--- app/src/main/java/com/davemcpherson/spotifystreamer/commands/SearchArtistCommand.java
+++ app/src/main/java/com/davemcpherson/spotifystreamer/commands/SearchArtistCommand.java
@@ -3,11 +3,16 @@ package com.davemcpherson.spotifystreamer.commands;
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by dave on 6/28/2015.
  */
 public class SearchArtistCommand extends SpotifyCommand {
+
+    private ArtistsPager artistsPager;
 
     public SearchArtistCommand(SpotifyService ss) {
         if(ss == null){
@@ -19,7 +24,18 @@ public class SearchArtistCommand extends SpotifyCommand {
 
 
     @Override
-    public ArtistsPager execute(String artist) {
-        return this.spotifyService.searchArtists(artist);
+    public ArtistsPager execute(String artistName) {
+        spotifyService.searchArtists(artistName, new Callback<ArtistsPager>() {
+            @Override
+            public void success(ArtistsPager ap, Response response) {
+                artistsPager = ap;
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                artistsPager = new ArtistsPager();
+            }
+        });
+        return artistsPager;
     }
 }
--- app/src/main/java/com/davemcpherson/spotifystreamer/commands/TopTracksCommand.java
+++ app/src/main/java/com/davemcpherson/spotifystreamer/commands/TopTracksCommand.java
@@ -4,11 +4,16 @@ import java.util.HashMap;
 
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Tracks;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by dave on 6/29/2015.
  */
 public class TopTracksCommand extends SpotifyCommand {
+
+    private Tracks tracks;
 
     public TopTracksCommand(SpotifyService ss){
         this.spotifyService = ss;
@@ -17,6 +22,18 @@ public class TopTracksCommand extends SpotifyCommand {
     public Tracks execute(String artist_id) {
         HashMap<String,Object> options = new HashMap<>();
         options.put("country","US");
-        return this.spotifyService.getArtistTopTrack(artist_id,options);
+        spotifyService.getArtistTopTrack(artist_id, options, new Callback<Tracks>() {
+
+            @Override
+            public void success(Tracks results, Response response) {
+                tracks = results;
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                tracks = new Tracks();
+            }
+        });
+        return tracks;
     }
 }
